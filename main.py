53506d6bfe7bb15c95a83e4e7b5f8466e8e730f4
Gabriel <contato@gabrielporfiro.com>
2021-12-15 19:56:45 -0300

    Checked

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 import socket
 
+def port_scan(host, port):
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((host, port))
+        print("Port %d is open" % port)
+        s.close()
+    except:
 
-def read_txt(path: str) -> str:
-    with open(path, 'r') as file:
-        return file.read().splitlines()
+        print("Port %d is closed" % port)
 
-
+port_scan('20.124.80.187', 3306)
 connection: socket.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 location: str = '20.124.80.187'
