bb9f51e01727ba5d7c7b95f15c50a458edf8df6f
Cristiliano Júnior <cristilianojunior@gmail.com>
2021-12-19 19:32:03 -0300

    Update

--- view/inputs.py
+++ view/inputs.py
@@ -10,4 +10,3 @@ def input_banner(text: str, simbol: str = '-', size: int = 50, text_foreground_c
                  line_background_color: str = 'default') -> str:
     banner(text, simbol, size, text_foreground_color, text_background_color, line_foreground_color, line_background_color)
     return inputc('>>> ', text_foreground_color, text_background_color)
-
