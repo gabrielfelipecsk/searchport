4afad07ee4744ae371712c6aec0ac12ee6bc3437
Cristiliano Júnior <cristilianojunior@gmail.com>
2021-12-19 21:09:49 -0300

    Update v0.0.4

--- main.py
+++ main.py
@@ -6,9 +6,32 @@ from controllers.connection import *
 from view.utilities import clear
 
 
+def add_new_host_ip() -> None:
+    response = create_new_data_host()
+
+def remove_host_ip() -> None:
+    pass
+
+def host_ip_list() -> None:
+    pass
+
+def init_port_scanner() -> None:
+    pass
+
+def settings() -> None:
+    pass
+
+def exit_app() -> None:
+    clear()
+    banner('Programa Encerrado!', title_foreground_color='red', line_foreground_color='red')
+    input('ENTER TO CONTINUE')
+    exit()
+    
+
 # Main Loop
 while True:
     clear()
+
     option_banner(title='Scanner Host IP', title_background_color='green', line_background_color='green',options=[
         {'text': 'Add New Host IP'},
         {'text': 'Remove Host Ip'},
@@ -21,8 +44,19 @@ while True:
     user_choice: str = inputc('>>> ', 'blue')
     
     if user_choice == '0':
+        add_new_host_ip()
+    elif user_choice == '1':
 
         input('ENTER TO CONTINUE')
-    elif user_choice == '1':
-        
+    elif user_choice == '2':
+
         input('ENTER TO CONTINUE')
+    elif user_choice == '3':
+
+        input('ENTER TO CONTINUE')
+    elif user_choice == '4':
+
+        input('ENTER TO CONTINUE')
+    elif user_choice == '5':
+        exit_app()
+        
